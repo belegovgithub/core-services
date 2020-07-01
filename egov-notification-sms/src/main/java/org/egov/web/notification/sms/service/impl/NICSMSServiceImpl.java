77508239d33e2fe90237fc5b828fddba83be23ff
Santhosh <santhups52@gmail.com>
2020-07-01 16:12:38 +0530

    TEXTLOCAL for demo

--- egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/NICSMSServiceImpl.java
+++ egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/NICSMSServiceImpl.java
@@ -1,61 +1,54 @@
-package org.egov.web.notification.sms.service.impl;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-import org.egov.web.notification.sms.config.SMSProperties;
-import org.egov.web.notification.sms.models.Sms;
-import org.egov.web.notification.sms.service.SMSService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Service;
-
-@Service
-@ConditionalOnProperty(value = "sms.gateway.to.use", havingValue ="NIC_SMS", matchIfMissing = true)
-public class NICSMSServiceImpl implements SMSService {
-	
-	@Autowired private SMSProperties smsProperties;	
-	
-	@Override
-	public void sendSMS(Sms sms) {
-
-		System.out.println("Inside Nic sms : " + smsProperties.getSenderid());
-
-		try {
-			String username = "username=" + smsProperties.getUsername();
-			String pin = "&pin=" + smsProperties.getPassword();
-			String message = "&message=" + sms.getMessage();
-			String mnumber = "&mnumber=91" + sms.getMobileNumber();
-			String signature = "&signature=" + smsProperties.getSenderid();
-
-			HttpURLConnection conn = (HttpURLConnection) new URL(smsProperties.getUrl()).openConnection();
-
-			String data = username + pin + message + mnumber + signature;
-			System.out.println("data " + data);
-
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
-			conn.getOutputStream().write(data.getBytes("UTF-8"));
-			final BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			final StringBuffer stringBuffer = new StringBuffer();
-			String line;
-			while ((line = rd.readLine()) != null) {
-				stringBuffer.append(line);
-			}
-			rd.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Error SMS ");
-
-		}
-
-	}
-
-}
-
-
-
+/*
+ * package org.egov.web.notification.sms.service.impl;
+ * 
+ * import java.io.BufferedReader; import java.io.InputStreamReader; import
+ * java.net.HttpURLConnection; import java.net.URL;
+ * 
+ * import org.egov.web.notification.sms.config.SMSProperties; import
+ * org.egov.web.notification.sms.models.Sms; import
+ * org.egov.web.notification.sms.service.SMSService; import
+ * org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+ * import org.springframework.stereotype.Service;
+ * 
+ * @Service
+ * 
+ * @ConditionalOnProperty(value = "sms.gateway.to.use", havingValue ="NIC_SMS",
+ * matchIfMissing = true) public class NICSMSServiceImpl implements SMSService {
+ * 
+ * @Autowired private SMSProperties smsProperties;
+ * 
+ * @Override public void sendSMS(Sms sms) {
+ * 
+ * System.out.println("Inside Nic sms : " + smsProperties.getSenderid());
+ * 
+ * try { String username = "username=" + smsProperties.getUsername(); String pin
+ * = "&pin=" + smsProperties.getPassword(); String message = "&message=" +
+ * sms.getMessage(); String mnumber = "&mnumber=91" + sms.getMobileNumber();
+ * String signature = "&signature=" + smsProperties.getSenderid();
+ * 
+ * HttpURLConnection conn = (HttpURLConnection) new
+ * URL(smsProperties.getUrl()).openConnection();
+ * 
+ * String data = username + pin + message + mnumber + signature;
+ * System.out.println("data " + data);
+ * 
+ * conn.setDoOutput(true); conn.setRequestMethod("POST");
+ * conn.setRequestProperty("Content-Length", Integer.toString(data.length()));
+ * conn.getOutputStream().write(data.getBytes("UTF-8")); final BufferedReader rd
+ * = new BufferedReader(new InputStreamReader(conn.getInputStream())); final
+ * StringBuffer stringBuffer = new StringBuffer(); String line; while ((line =
+ * rd.readLine()) != null) { stringBuffer.append(line); } rd.close();
+ * 
+ * } catch (Exception e) { e.printStackTrace();
+ * System.out.println("Error SMS ");
+ * 
+ * }
+ * 
+ * }
+ * 
+ * }
+ * 
+ * 
+ * 
+ */
--- egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/TextLocalImpl.java
+++ egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/TextLocalImpl.java
@@ -5,29 +5,25 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.egov.web.notification.sms.config.SMSProperties;
 import org.egov.web.notification.sms.models.Sms;
 import org.egov.web.notification.sms.service.SMSService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 @Service
-@ConditionalOnProperty(value = "sms.gateway.to.use", havingValue ="TEXTLOCAL_SMS", matchIfMissing = true)
+@ConditionalOnProperty(value = "sms.gateway.to.use", havingValue ="DEFAULT", matchIfMissing = true)
 public class TextLocalImpl implements SMSService {
-	
-	@Autowired private SMSProperties smsProperties;	
-	
+
 	@Override
 	public void sendSMS(Sms sms) {
 		// TODO Auto-generated method stub
 		try {
 			// Construct data
-		    String apiKey = "apikey=" + smsProperties.getSecureKey();
+		    String apiKey = "apikey=" + "YW7j9bfyV6A-FDzcLlUcEbQMFLCkhmu5C0fz6wca3i";
 			String message = "&message=" + sms.getMessage();
-			String sender = "&sender=" + smsProperties.getSenderid();
+			String sender = "&sender=" + "TXTLCL";
 			String numbers = "&numbers=" +"91"+ sms.getMobileNumber();
 			// Send data"91"
-			HttpURLConnection conn = (HttpURLConnection) new URL(smsProperties.getUrl()).openConnection();
+			HttpURLConnection conn = (HttpURLConnection) new URL("https://api.textlocal.in/send/?").openConnection();
 			String data = apiKey + numbers + message + sender;
 			System.out.println("data "+data);
 			conn.setDoOutput(true);
