23b8d62ed487984e9a1a98df592c8df7b20ffd50
divyachandran <chandran.divya07@gmail.com>
2020-08-21 16:19:41 +0530

    user otp 3 attempts

---
+++ user-otp/src/main/java/org/egov/domain/exception/TokenGenerationAttemptsOverException.java
@@ -0,0 +1,8 @@
+package org.egov.domain.exception;
+
+public class TokenGenerationAttemptsOverException extends RuntimeException {
+
+	
+	private static final long serialVersionUID = -7682510712584181850L;
+
+}
--- user-otp/src/main/java/org/egov/persistence/repository/OtpRepository.java
+++ user-otp/src/main/java/org/egov/persistence/repository/OtpRepository.java
@@ -2,11 +2,17 @@ package org.egov.persistence.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import org.egov.domain.exception.OtpNumberNotPresentException;
+import org.egov.domain.exception.TokenGenerationAttemptsOverException;
 import org.egov.domain.model.OtpRequest;
 import org.egov.persistence.contract.OtpResponse;
+import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
+import org.egov.common.contract.response.Error;
 
 @Service
 @Slf4j
@@ -23,17 +29,27 @@ public class OtpRepository {
     }
 
     public String fetchOtp(OtpRequest otpRequest) {
-        final org.egov.persistence.contract.OtpRequest request =
-                new org.egov.persistence.contract.OtpRequest(otpRequest);
-        try {
+    	try {
+    		final org.egov.persistence.contract.OtpRequest request =
+                    new org.egov.persistence.contract.OtpRequest(otpRequest);
             final OtpResponse otpResponse =
                     restTemplate.postForObject(otpCreateUrl, request, OtpResponse.class);
-            if (isOtpNumberAbsent(otpResponse)) {
+
+            if(isOtpNumberAbsent(otpResponse)) {
                 throw new OtpNumberNotPresentException();
             }
             return otpResponse.getOtpNumber();
-        } catch (Exception e) {
-            log.error("Exception while fetching OTP: ", e);
+    	}
+    	
+    	catch (HttpClientErrorException e) {    		   		
+    		if(HttpStatus.CONFLICT.equals(e.getStatusCode()))
+    		   throw new TokenGenerationAttemptsOverException();
+    		else
+    			 throw new OtpNumberNotPresentException();
+		}
+    	catch(HttpStatusCodeException  e) {
+			throw new CustomException(e.getStatusCode().toString(),e.getStatusText());
+		} catch (Exception e) {
             throw new OtpNumberNotPresentException();
         }
     }
