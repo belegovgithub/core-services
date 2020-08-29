39f95fef113a3174dd4de397c3ca6ab3cde9d633
belegovgithub <belegovgithub@gmail.com>
2020-08-29 17:29:55 +0530

    zuul update

--- zuul/src/main/java/org/egov/config/Configuration.java
+++ zuul/src/main/java/org/egov/config/Configuration.java
@@ -1,16 +1,11 @@
 package org.egov.config;
 
-import org.egov.exceptions.CustomException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.DefaultRateLimiterErrorHandler;
-import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.RateLimiterErrorHandler;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
@@ -26,25 +21,6 @@ public class Configuration {
         return new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
     }
 
-    @Bean
-    public RateLimiterErrorHandler rateLimitErrorHandler() {
-      return new DefaultRateLimiterErrorHandler() {
-          @Override
-          public void handleSaveError(String key, Exception e) {
-              throw new RuntimeException( new CustomException("TOO_MANY_REQUESTS", HttpStatus.TOO_MANY_REQUESTS.value() ,"Too many requests"));
-          }
 
-          @Override
-          public void handleFetchError(String key, Exception e) {
-        	  throw new RuntimeException( new CustomException("TOO_MANY_REQUESTS", HttpStatus.TOO_MANY_REQUESTS.value() ,"Too many requests"));
-          }
-
-          @Override
-          public void handleError(String msg, Exception e) {
-        	  throw new RuntimeException( new CustomException("TOO_MANY_REQUESTS", HttpStatus.TOO_MANY_REQUESTS.value() ,"Too many requests"));
-          }
-      };
-    }
-    
 
 }
