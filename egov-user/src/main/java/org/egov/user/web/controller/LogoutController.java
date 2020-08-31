b5484a24f63f190ac26a879932ce3f7f2f483a4c
ragini <pb.ragini@gmail.com>
2020-08-31 10:48:53 +0530

    logout controller changed to avoid accessing access token in url

--- egov-user/src/main/java/org/egov/user/web/controller/LogoutController.java
+++ egov-user/src/main/java/org/egov/user/web/controller/LogoutController.java
@@ -3,12 +3,14 @@ package org.egov.user.web.controller;
 import org.egov.common.contract.response.Error;
 import org.egov.common.contract.response.ErrorResponse;
 import org.egov.common.contract.response.ResponseInfo;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +33,12 @@ public class LogoutController {
      * @throws Exception
      */
     @PostMapping("/_logout")
-    public ResponseInfo deleteToken(@RequestParam("access_token") String accessToken) throws Exception {
-        OAuth2AccessToken redisToken = tokenStore.readAccessToken(accessToken);
+    public ResponseInfo deleteToken(@RequestBody String request) throws Exception {
+    	System.out.println("request Info"+request);
+    	JSONObject obj = new JSONObject(request);
+    	System.out.println("access token"+obj.get("access_token"));
+    	OAuth2AccessToken redisToken = tokenStore.readAccessToken(obj.get("access_token").toString());
+       // OAuth2AccessToken redisToken = tokenStore.readAccessToken(access_token);
         tokenStore.removeAccessToken(redisToken);
         return new ResponseInfo("", "", new Date().toString(), "", "", "Logout successfully");
     }
