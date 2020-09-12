307d4c3f34915d8fe185ecc2ab8ffdbef30c9652
santhups52 <santhups52@gmail.com>
2020-09-12 10:18:12 +0530

    Mail Implementation changed to get Server Response

--- egov-notification-mail/src/main/java/org/egov/web/notification/mail/service/ExternalEmailService.java
+++ egov-notification-mail/src/main/java/org/egov/web/notification/mail/service/ExternalEmailService.java
@@ -1,14 +1,24 @@
 package org.egov.web.notification.mail.service;
 
+import java.util.Properties;
+
+import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.egov.web.notification.mail.config.EmailProperties;
 import org.egov.web.notification.mail.consumer.contract.Email;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import com.sun.mail.smtp.SMTPTransport;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,24 +26,54 @@ import lombok.extern.slf4j.Slf4j;
 @ConditionalOnProperty(value = "mail.enabled", havingValue = "true")
 @Slf4j
 public class ExternalEmailService implements EmailService {
+	
+	@Autowired
+    private EmailProperties emailProperties;
 
 	public static final String EXCEPTION_MESSAGE = "Exception creating HTML email";
-	private JavaMailSenderImpl mailSender;
+	//private JavaMailSenderImpl mailSender;
 
     public ExternalEmailService(JavaMailSenderImpl mailSender) {
-        this.mailSender = mailSender;
+        //this.mailSender = mailSender;
     }
     
     @Override
-    public void sendEmail(Email email) {
-		if(email.isHTML()) {
-			sendHTMLEmail(email);
-		} else {
-			sendTextEmail(email);
+	public void sendEmail(Email email) {
+		/*
+		 * if(email.isHTML()) { sendHTMLEmail(email); } else { sendTextEmail(email); }
+		 */
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.smtp.host", emailProperties.getMailHost());
+		properties.setProperty("mail.smtp.port", emailProperties.getMailPort().toString());
+		properties.setProperty("mail.protocol", emailProperties.getMailProtocol());
+		properties.setProperty("mail.smtps.starttls.enable", emailProperties.getMailStartTlsEnabled());
+		properties.setProperty("mail.smtp.auth",emailProperties.getMailSmtpsAuth());
+		properties.setProperty("mail.user", emailProperties.getMailSenderUsername());
+		properties.setProperty("mail.password", emailProperties.getMailSenderPassword());
+		properties.setProperty("mail.smtps.debug", emailProperties.getMailSmtpsDebug());
+		Session session = Session.getDefaultInstance(properties);
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(emailProperties.getMailSenderUsername()));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(String.join(",", email.getEmailTo())));
+			message.setSubject(email.getSubject());
+			message.setText(email.getBody());
+			Transport tp = session.getTransport(emailProperties.getMailProtocol());
+			tp.connect(emailProperties.getMailHost(),emailProperties.getMailPort(),
+				emailProperties.getMailSenderUsername(),emailProperties.getMailSenderPassword());
+			tp.sendMessage(message, message.getAllRecipients());
+			log.info("Response 1 from server : " + ((SMTPTransport) tp).getLastServerResponse());
+			log.info("Response 2 from server : " + ((SMTPTransport) tp).getLastReturnCode());
+			tp.close();
+			log.info("Mail sent successfully....");
+		} catch (Exception e) {
+			log.error("Error sending in mail.", e);
+			throw new RuntimeException(e);
 		}
-    }
 
-	private void sendTextEmail(Email email) {
+	}
+
+	/*private void sendTextEmail(Email email) {
 		try {
 			final SimpleMailMessage mailMessage = new SimpleMailMessage();
 			mailMessage.setTo(email.getEmailTo().toArray(new String[0]));
@@ -42,6 +82,7 @@ public class ExternalEmailService implements EmailService {
 			log.info("Text Mail sending to : "+email.getEmailTo().size());
 			mailMessage.setFrom(mailSender.getUsername());
 			mailSender.send(mailMessage);
+			log.info("Mail sent");
 		}
 		catch(Exception e) {
 			log.error("Error sending in mail."+e);
@@ -61,5 +102,5 @@ public class ExternalEmailService implements EmailService {
 			throw new RuntimeException(e);
 		}
 		mailSender.send(message);
-	}
+	}*/
 }
