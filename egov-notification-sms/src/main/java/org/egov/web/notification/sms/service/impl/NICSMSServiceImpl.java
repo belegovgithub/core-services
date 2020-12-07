dabb2ac9ecaa26be7f1d93cbae7c40e556c954e9
belegovgithub <belegovgithub@gmail.com>
2020-12-07 17:16:47 +0530

    null check for templateid

--- egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/NICSMSServiceImpl.java
+++ egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/NICSMSServiceImpl.java
@@ -58,8 +58,10 @@ public class NICSMSServiceImpl extends BaseSMSService {
         	final_data+="&mnumber=91"+ sms.getMobileNumber();
         	final_data+="&signature="+ smsProperties.getSenderid();
         	final_data+="&dlt_entity_id="+ smsProperties.getSmsEntityId();
-        	final_data+="&dlt_template_id="+ sms.getTemplateId();
-        	log.info("dlt_entity_id "+smsProperties.getSmsEntityId());
+			if(null != sms.getTemplateId())
+			{
+				final_data+="&dlt_template_id="+ sms.getTemplateId();
+			}
         	SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
         	if(smsProperties.isVerifyCertificate()) {
         		log.info("checking certificate");
