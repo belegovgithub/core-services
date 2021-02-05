28cc7512cb2f3f5a40ee50116518ef85f292c79e
nvcs057 <nvcs057@gmail.com>
2021-02-05 10:34:29 +0530

    added code for amount in words

--- pdf-service/src/utils/directMapping.js
+++ pdf-service/src/utils/directMapping.js
@@ -267,7 +267,32 @@ export const directMapping = async (
           tenantId
         );
       }
-    }else if (directArr[i].type == "date") {
+    }
+    else if (directArr[i].type == "amountInWords") 
+    {
+      console.log("its in for words");
+      console.log("jpath",directArr[i].jPath);
+      console.log("value--",directArr[i].val);
+
+    let a = ['','One ','Two ','Three ','Four ', 'Five ','Six ','Seven ','Eight ','Nine ','Ten ','Eleven ','Twelve ','Thirteen ','Fourteen ','Fifteen ','Sixteen ','Seventeen ','Eighteen ','Nineteen '];
+    let b = ['', '', 'Twenty','Thirty','Forty','Fifty', 'Sixty','Seventy','Eighty','Ninety'];
+    let finalStr = '';
+
+    if ((directArr[i].val = directArr[i].val.toString()).length > 9) 
+    finalStr = "NA";
+    let n = ('000000000' + directArr[i].val).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+    if (!n) finalStr = "NA"; 
+    let str = '';
+    str += (n[1] != 0) ? (a[Number(n[1])] || b[n[1][0]] + ' ' + a[n[1][1]]) + 'Crore ' : '';
+    str += (n[2] != 0) ? (a[Number(n[2])] || b[n[2][0]] + ' ' + a[n[2][1]]) + 'Lakh ' : '';
+    str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'Thousand ' : '';
+    str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'Hundred ' : '';
+    str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) + 'Rupees Only ' : '';
+    finalStr = str;
+    console.log("final String--",finalStr);
+    variableTovalueMap[directArr[i].jPath] = finalStr;
+    }
+    else if (directArr[i].type == "date") {
       let myDate = new Date(directArr[i].val[0]);
       if (isNaN(myDate) || directArr[i].val[0] === 0) {
         variableTovalueMap[directArr[i].jPath] = "NA";
