ed16aaaf9ceefe5b51b6c0e0ad6d46dc599bf60c
Rajanihn <rajnihn@gmail.com>
2021-03-30 11:10:48 +0530

    User validation based on mobile number instead of uuid

--- egov-workflow-v2/src/main/java/org/egov/wf/validator/WorkflowValidator.java
+++ egov-workflow-v2/src/main/java/org/egov/wf/validator/WorkflowValidator.java
@@ -177,9 +177,9 @@ public class WorkflowValidator {
             if(requestInfo.getUserInfo().getType().equalsIgnoreCase(CITIZEN_TYPE)){
                 ProcessInstance processInstanceFromDB = processStateAndAction.getProcessInstanceFromDb();
                 if(processInstanceFromDB!=null && processInstanceFromDB.getAction().equalsIgnoreCase(SENDBACKTOCITIZEN)){
-                    List<String> assignes = processInstanceFromDB.getAssignes().stream().map(User::getUuid).collect(Collectors.toList());
-                    if(!assignes.contains(requestInfo.getUserInfo().getUuid()))
-                        throw new CustomException("INVALID_USER","The user: "+requestInfo.getUserInfo().getUuid()+" is not authorized to take action");
+                    List<String> assignes = processInstanceFromDB.getAssignes().stream().map(User::getMobileNumber).collect(Collectors.toList());
+                    if(!assignes.contains(requestInfo.getUserInfo().getMobileNumber()))
+                        throw new CustomException("INVALID_USER","The user: "+requestInfo.getUserInfo().getMobileNumber()+" is not authorized to take action");
                 }
             }
 
